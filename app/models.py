90b51a70bb558bf27fc139cccfd95d26a5b586a4
pedrolisboaa <phlisboa2000@gmail.com>
2023-12-07 16:39:07 -0300

    começo do banco do projeto

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+
+# Create your models here.
+class Endereco(models.Model):
+    cep = models.CharField(max_length=9)
+    cidade = models.CharField(max_length=150)
+    estados = models.CharField(max_length=50)
+    complemento = models.CharField(max_length=100)
+    
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    cpf = models.CharField(max_length=11, null=False, blank=False, unique=True)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    endereco = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+    data_de_nascimento = models.DateField(null=False, blank=False)
+    data_de_cadastro = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.nome
