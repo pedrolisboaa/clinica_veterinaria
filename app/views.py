cd3ed835a105d2b665500977d8639a2e24343593
pedrolisboaa <phlisboa2000@gmail.com>
2023-12-08 17:24:36 -0300

    atualizando

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -14,7 +14,7 @@ def index(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return('home')
+            return redirect('home')
         else:
             messages.error(request, 'Usuário ou senha incorretos.')
     
