f9d3c83c5c078614f2935b91996ce01d342e27cf
pedrolisboaa <phlisboa2000@gmail.com>
2023-12-11 17:28:00 -0300

    atualizando projetinho

--- app/forms.py
+++ app/forms.py
@@ -7,10 +7,21 @@ class EnderecoForm(forms.ModelForm):
     class Meta:
         model = Endereco
         fields = ['cep', 'cidade', 'estado', 'complemento',]
+        widgets = {
+            'cep': forms.TextInput(attrs={'class': 'form-control'}),
+            'cidade': forms.TextInput(attrs={'class': 'form-control'}),
+            'estado': forms.TextInput(attrs={'class': 'form-control'}),
+            'complemento': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 
 class TutorForm(forms.ModelForm):
     class Meta:
         model = Tutor
         fields = ['nome', 'cpf', 'email', 'data_de_nascimento',]
-
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome Completo'}),
+            'cpf': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'CPF'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'E-mail'}),
+            'data_de_nascimento': forms.DateInput(attrs={'class': 'form-control','type':'date' ,'placeholder': 'Data denascimento'}),
+        }
